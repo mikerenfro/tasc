47b691c9131ec4b4b8fdc9ee86a8ed4c733dad8e
Mike Renfro <renfro@tntech.edu>
2019-01-13 12:07:30 -0600

    Cleaning up plt_lppl_int.m

--- plt_lppl_int.m
+++ plt_lppl_int.m
@@ -13,6 +13,7 @@ sigma_l = e_t_l.*E;
 sigma_nl = Sys.*(e_t_nl/eys).^(1/n);
 str1 = 'LPPL Equation';
 str2 = 'Data Table';
+epl = zeros(size(e_t_nl));
 for i = 1:length(e_t_nl)
     epl(i) = e_t_nl(i) - sigma_nl(i)/E;
 end
@@ -20,62 +21,64 @@ end
 %do logical test for strain axis type
 
 if handles.interp.se_strain_type_flag == 1
-
+    
     if handles.interp.se_axes_type_flag == 1
-    plot(epl, sigma_nl, '-r');
+        plot(epl, sigma_nl, '-r');
     else
-    loglog(epl, sigma_nl, '-r');
+        loglog(epl, sigma_nl, '-r');
     end
-   
+    
     hold on
     legend_text = {str1};
     xlabel('Plastic Strain');
     ylabel(handles.result.stress_str);
-
+    
     if se_table(1,1) ~= 0 && handles.interp.se_data_plot_flag == 1
         if handles.interp.se_axes_type_flag == 1
-        plot(se_table(:,2), se_table(:,1), '--xb');
+            plot(se_table(:,2), se_table(:,1), '--xb');
         else
-        loglog(se_table(:,2), se_table(:,1), '--xb');
+            loglog(se_table(:,2), se_table(:,1), '--xb');
         end
-
-    legend_text = {str1, str2};
+        
+        legend_text = {str1, str2};
     end
 else
+    e_total = zeros(size(e_t_nl));
     for i = 1:length(e_t_nl)
-    e_total(i) = epl(i) + sigma_nl(i)/E;
+        e_total(i) = epl(i) + sigma_nl(i)/E;
     end
     if handles.interp.se_axes_type_flag == 1
         plot(e_total, sigma_nl, '-r');
-        else
+    else
         loglog(e_total, sigma_nl, '-r');
     end
     hold on
     legend_text = {str1};
     xlabel('Strain');
     ylabel(handles.result.stress_str);
-
+    
     if se_table(1,1) ~= 0 && handles.interp.se_data_plot_flag == 1
+        se_total = zeros(size(se_table));
         for i = 1:length(se_table)
             se_total(i) = se_table(i,2)+ se_table(i,1)/E;
         end
-    if handles.interp.se_axes_type_flag == 1
-    plot(se_total, se_table(:,1), '--xb');
-    else
-    loglog(se_total, se_table(:,1), '--xb');
-    end
-
-    legend_text = {str1, str2};
+        if handles.interp.se_axes_type_flag == 1
+            plot(se_total, se_table(:,1), '--xb');
+        else
+            loglog(se_total, se_table(:,1), '--xb');
+        end
+        
+        legend_text = {str1, str2};
     end
     %plot elastic portions of data
     if handles.interp.se_axes_type_flag == 1
-    plot([0 sigma_l(2)/E],[0 sigma_l(2)],'-r')
+        plot([0 sigma_l(2)/E],[0 sigma_l(2)],'-r')
     else
         loglog([0 sigma_l(2)/E],[0 sigma_l(2)],'-r');
     end
     if se_table(1,1) ~= 0 && handles.interp.se_data_plot_flag == 1
         if handles.interp.se_axes_type_flag == 1
-        plot([0 se_table(1,1)/E], [0 se_table(1,1)],'--xb');
+            plot([0 se_table(1,1)/E], [0 se_table(1,1)],'--xb');
         else
             loglog([0 se_table(1,1)/E], [0 se_table(1,1)],'--xb');
         end

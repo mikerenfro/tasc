06a6493bb8e5db8727ba01008de9a9a5095e7427
Mike Renfro <renfro@tntech.edu>
2019-01-13 12:25:09 -0600

    Cleaning up get_interp_values_int.m

--- get_interp_values_int.m
+++ get_interp_values_int.m
@@ -32,28 +32,28 @@ if handles.interp.extrap_flag == 1
     handles.interp.extrap_factor = str2double(get(handles.et_i_extrap_cmod,   'String'));
 end
 %get test evaluation variables
- handles.interp.cb_eval_test = get(handles.cb_eval_test, 'value');
- if handles.interp.cb_eval_test == 1
-     handles.result.tear_load = str2double(get(handles.et_i_tear_force,   'String'));
-     handles.result.tear_angle = str2double(get(handles.et_i_tear_angle,   'String'));
-%  else
-%      handles.result.tear_load = [];
-%      handles.result.tear_angle = [];
-%      handles.testdata.CMOD = 0;
-%      handles.testdata.force = 0;
-%      handles.testdata.testdata_filename = [];
- end
- %get pre-test prediction variables
- handles.interp.cb_test_predict = get(handles.cb_test_predict, 'value');
- if handles.interp.cb_test_predict == 1
+handles.interp.cb_eval_test = get(handles.cb_eval_test, 'value');
+if handles.interp.cb_eval_test == 1
+    handles.result.tear_load = str2double(get(handles.et_i_tear_force,   'String'));
+    handles.result.tear_angle = str2double(get(handles.et_i_tear_angle,   'String'));
+    %  else
+    %      handles.result.tear_load = [];
+    %      handles.result.tear_angle = [];
+    %      handles.testdata.CMOD = 0;
+    %      handles.testdata.force = 0;
+    %      handles.testdata.testdata_filename = [];
+end
+%get pre-test prediction variables
+handles.interp.cb_test_predict = get(handles.cb_test_predict, 'value');
+if handles.interp.cb_test_predict == 1
     handles.result.crit_Jc = str2double(get(handles.et_crit_Jc,   'String'));
     handles.result.crit_phi = str2double(get(handles.et_crit_phi,   'String'));
- else
-     handles.result.crit_Jc = [];
+else
+    handles.result.crit_Jc = [];
     handles.result.crit_phi = [];
- end
+end
 %get save plots check box value
- handles.interp.cb_save_plots = get(handles.cb_save_plots, 'value');
+handles.interp.cb_save_plots = get(handles.cb_save_plots, 'value');
 %%
 %perform error checking on inputs
 ErrorFound = 0;
@@ -81,10 +81,10 @@ if handles.interp.cb_test_predict == 1
     %ensure angle is between 0 and 90
     if (ta <= 0 || ta > 90)
         set(handles.et_crit_phi, 'BackgroundColor', [0.86  0.275  0.275]);
-        er_box = errordlg('Crit. angle only valid for 0 < phi <= 90');
+        errordlg('Crit. angle only valid for 0 < phi <= 90');
         ErrorFound = 1;
-%     else
-%         set(handles.et_i_tear_angle, 'BackgroundColor', 'w');
+        %     else
+        %         set(handles.et_i_tear_angle, 'BackgroundColor', 'w');
     end
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -103,12 +103,12 @@ if handles.interp.cb_eval_test == 1
     end
     %ensure tear force is not greater than test data limit
     td = handles.testdata.force;
-    if (length(td) > 1) && (tf > max(td)*1.0) 
+    if (length(td) > 1) && (tf > max(td)*1.0)
         set(handles.et_i_tear_force, 'BackgroundColor', [0.86  0.275  0.275]);
-        er_box = errordlg('Tear force is greater than max. force in test record. Enter a smaller value.');
+        errordlg('Tear force is greater than max. force in test record. Enter a smaller value.');
         ErrorFound = 1;
-%     else
-%         set(handles.et_i_tear_force, 'BackgroundColor', 'w');
+        %     else
+        %         set(handles.et_i_tear_force, 'BackgroundColor', 'w');
     end
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -123,10 +123,10 @@ if handles.interp.cb_eval_test == 1
     %ensure angle is between 0 and 90
     if (ta <= 0 || ta > 90)
         set(handles.et_i_tear_angle, 'BackgroundColor', [0.86  0.275  0.275]);
-        er_box = errordlg('Tear angle only valid for 0 < phi <= 90');
+        errordlg('Tear angle only valid for 0 < phi <= 90');
         ErrorFound = 1;
-%     else
-%         set(handles.et_i_tear_angle, 'BackgroundColor', 'w');
+        %     else
+        %         set(handles.et_i_tear_angle, 'BackgroundColor', 'w');
     end
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -140,7 +140,7 @@ if handles.interp.cb_eval_test == 1
     end
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %check error factor
     ef = handles.interp.err_factor;
     if (ef <= 0 || isnan(ef) || isempty(ef))
@@ -152,7 +152,7 @@ if handles.interp.cb_eval_test == 1
     %ensure error factor is between 0 and 100
     if (ef <= 0 || ef > 100)
         set(handles.et_err_factor, 'BackgroundColor', [0.86  0.275  0.275]);
-        er_box = errordlg('Percent error factor only valid for 0 < error_factor <= 100');
+        errordlg('Percent error factor only valid for 0 < error_factor <= 100');
         ErrorFound = 1;
     else
         set(handles.et_err_factor, 'BackgroundColor', 'w');
@@ -227,8 +227,8 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %check extrap factor
 if handles.interp.extrap_flag == 1
-cf = handles.interp.extrap_factor;
-    if ( isnan(cf) || isempty(cf)) 
+    cf = handles.interp.extrap_factor;
+    if ( isnan(cf) || isempty(cf))
         set(handles.et_i_extrap_cmod, 'BackgroundColor', [0.86  0.275  0.275]);
         ErrorFound = 1;
     else
@@ -245,43 +245,40 @@ cn = handles.interp.n;
 if cn < 3.0 || cn > 20.0
     ErrorFound = 1;
     set(handles.et_i_n, 'BackgroundColor', [0.86  0.275  0.275]);
-    er_box = errordlg('Interpolation is only valid for 3.0 <= n <= 20');
-else if ErrorFound == 0;
+    errordlg('Interpolation is only valid for 3.0 <= n <= 20');
+elseif ErrorFound == 0
     set(handles.et_i_n, 'BackgroundColor', 'w');
-    end
 end
+
 %
 if ac < 0.2 || ac > 1.0
     ErrorFound = 1;
     set(handles.et_i_a, 'BackgroundColor', [0.86  0.275  0.275]);
     set(handles.et_i_2c, 'BackgroundColor', [0.86  0.275  0.275]);
-    er_box = errordlg('Interpolation is only valid for 0.2 <= a/c <= 1.0');
-else if ErrorFound == 0;
+    errordlg('Interpolation is only valid for 0.2 <= a/c <= 1.0');
+elseif ErrorFound == 0
     set(handles.et_i_a, 'BackgroundColor', 'w');
     set(handles.et_i_2c, 'BackgroundColor', 'w');
-    end
 end
 %
 if aB < 0.2 || aB > 0.80
     ErrorFound = 1;
     set(handles.et_i_a, 'BackgroundColor', [0.86  0.275  0.275]);
     set(handles.et_i_B, 'BackgroundColor', [0.86  0.275  0.275]);
-    er_box = errordlg('Interpolation is only valid for 0.2 <= a/B <= 0.8');
-else if ErrorFound == 0;
+    errordlg('Interpolation is only valid for 0.2 <= a/B <= 0.8');
+elseif ErrorFound == 0
     set(handles.et_i_a, 'BackgroundColor', 'w');
     set(handles.et_i_B, 'BackgroundColor', 'w');
-    end
 end
 %
 if ESys < 100 || ESys > 1000
     ErrorFound = 1;
     set(handles.et_i_E, 'BackgroundColor', [0.86  0.275  0.275]);
     set(handles.et_i_Sys, 'BackgroundColor', [0.86  0.275  0.275]);
-    er_box = errordlg('Interpolation is only valid for 100 <= E/Sys <= 1000');
-else if ErrorFound == 0;
+    errordlg('Interpolation is only valid for 100 <= E/Sys <= 1000');
+elseif ErrorFound == 0
     set(handles.et_i_E, 'BackgroundColor', 'w');
     set(handles.et_i_Sys, 'BackgroundColor', 'w');
-    end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Check to see if everything is correct

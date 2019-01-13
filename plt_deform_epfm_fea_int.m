490d6b475f39e16f02ad03d34a1c9382c1a5fd74
Mike Renfro <renfro@tntech.edu>
2019-01-13 12:04:41 -0600

    Cleaning up plt_deform_epfm_fea_int.m

--- plt_deform_epfm_fea_int.m
+++ plt_deform_epfm_fea_int.m
@@ -4,7 +4,7 @@ function plt_deform_epfm_fea_int(result)
 % Get data from Result to plot easier
 predict_test = result.predict_test;
 eval_test = result.eval_test;
-if predict_test == 1 || eval_test == 1 ;
+if predict_test == 1 || eval_test == 1
     phi_tear = result.tear_angle;
 else
     phi_tear = 30.0; %set equal to 30 deg. for generic plot without test data
@@ -16,7 +16,7 @@ M_epfm_a = result.fea.M_epfm_a;
 M_epfm_b = result.fea.M_epfm_b;
 CK = result.fea.CK;
 CJa = result.fea.CJa;
-CJb = result.fea.CJb;    
+CJb = result.fea.CJb;
 M_epfm_a_tear = result.fea.M_epfm_a_tear;
 M_epfm_b_tear = result.fea.M_epfm_b_tear;
 J_tear_phi = result.fea.J_tear_phi;
@@ -24,34 +24,34 @@ Jel_tear_phi = result.fea.Jel_tear_phi;
 %calc percent J plastic
 Jpl_per = 100*(Jt-Jel)./Jt;
 %find phi result closest to phi_tear
-[C,I] = min(abs(phi_fea-phi_tear));
+[~,I] = min(abs(phi_fea-phi_tear));
 phi_str = num2str(phi_fea(I), '%5.2f');
 
 semilogx(M_epfm_a(I,:),Jpl_per(I,:),'-ob')
-   ylabel('%J_{plastic}');
-    xlabel('r*Sys/J'); 
+ylabel('%J_{plastic}');
+xlabel('r*Sys/J');
 %    title('EPFM Deformation Measure - FEA');
-    hold on
-  
- semilogx(M_epfm_b(I,:),Jpl_per(I,:),'-or','MarkerFaceColor','r')
- str1 = strcat('r_{\phia} \phi =', phi_str);
- str2 = strcat('r_{\phib} \phi =', phi_str);
- str3 = 'r_{\phia} \phi = 90.0';
- str4 = 'r_{\phib} \phi = 90.0';
- str5 = 'LEFM Deform Limit, C_K';
- str6 = 'EPFM Deform Limit, C_{Ja}';
- str7 = 'EPFM Deform Limit, C_{Jb}';
- str8 = 'Tearing Load Point';
+hold on
+
+semilogx(M_epfm_b(I,:),Jpl_per(I,:),'-or','MarkerFaceColor','r')
+str1 = strcat('r_{\phia} \phi =', phi_str);
+str2 = strcat('r_{\phib} \phi =', phi_str);
+str3 = 'r_{\phia} \phi = 90.0';
+str4 = 'r_{\phib} \phi = 90.0';
+str5 = 'LEFM Deform Limit, C_K';
+str6 = 'EPFM Deform Limit, C_{Ja}';
+str7 = 'EPFM Deform Limit, C_{Jb}';
+str8 = 'Tearing Load Point';
 %*update* *V1.0.2* 06/10/14
-%change phi = 90 results to gray color  
- L = size(phi_fea,2);
- semilogx(M_epfm_a(L,:),Jpl_per(L,:),'-s','color', [.7,.7,.7])
- semilogx(M_epfm_b(L,:),Jpl_per(L,:),'-s','color', [.7,.7,.7],'MarkerFaceColor',[.7,.7,.7])
+%change phi = 90 results to gray color
+L = size(phi_fea,2);
+semilogx(M_epfm_a(L,:),Jpl_per(L,:),'-s','color', [.7,.7,.7])
+semilogx(M_epfm_b(L,:),Jpl_per(L,:),'-s','color', [.7,.7,.7],'MarkerFaceColor',[.7,.7,.7])
 %  semilogx(M_epfm_a(L,:),Jpl_per(L,:),'-sb')
 %  semilogx(M_epfm_b(L,:),Jpl_per(L,:),'-sr','MarkerFaceColor','r')
- max1 = max(Jpl_per(I,:));
- max2 = max(Jpl_per(L,:));
- maximum = max(max1,max2);
+max1 = max(Jpl_per(I,:));
+max2 = max(Jpl_per(L,:));
+maximum = max(max1,max2);
 x = [CK CK];
 y = [0 maximum];
 x2 = [CJa CJa];
@@ -69,13 +69,7 @@ if M_epfm_a_tear ~= 0
 else
     legend_text = {str1, str2, str3, str4, str5, str6, str7};
 end
-    legend(legend_text, 'Location', 'NorthEast');
+legend(legend_text, 'Location', 'NorthEast');
 %set(gca,'XLim',[1 Inf])
 set(gca,'YLim',[0 maximum*1.05])
-   hold off
-   
-
- 
-
-
-   
+hold off

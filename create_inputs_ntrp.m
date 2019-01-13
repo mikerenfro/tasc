c9e3d277afa31052a61747e93032c5702f5823d7
Mike Renfro <renfro@tntech.edu>
2019-01-13 12:08:14 -0600

    Cleaning up create_inputs_ntrp.m

--- create_inputs_ntrp.m
+++ create_inputs_ntrp.m
@@ -10,7 +10,7 @@ txt_cell{2,1} = '%-----File generated from solution inputs-----%';
 txt_cell{3,1} = '%words proceeded with * are keywords for the';
 txt_cell{4,1} = '%text scanning program';
 %write units string
-if result.units == 1;
+if result.units == 1
     ustr = 'US';
 else
     ustr = 'SI';
@@ -19,7 +19,7 @@ txt_cell{5,1} = '%---Set Units----% (options are SI or US)';
 replace_str = ['*units ' ustr];
 txt_cell{6,1} = replace_str;
 %write geometry values
-tear_load_type = 2; %tension loading
+% tear_load_type = 2; %tension loading
 a = result.fea.a;
 c = result.fea.c;
 two_c = 2*c;
@@ -49,50 +49,50 @@ txt_cell{16,1} = replace_str;
 txt_cell{17,1} = '*end_material';
 %
 %write test prediction values
- if interp.cb_test_predict == 1
-     Jc = result.crit_Jc;
-     crit_phi = result.crit_phi;
-     txt_cell{18,1} = '%---Pre-test Predicion Values----%';
-     txt_cell{19,1} = '*pretest';
-     replace_str = ['*Jc ' num2str(Jc,'%10.3f')];
-     txt_cell{20,1} = replace_str;
-     replace_str = ['*phi_crit ' num2str(crit_phi,'%10.3f')];
-     txt_cell{21,1} = replace_str;
- end
+if interp.cb_test_predict == 1
+    Jc = result.crit_Jc;
+    crit_phi = result.crit_phi;
+    txt_cell{18,1} = '%---Pre-test Predicion Values----%';
+    txt_cell{19,1} = '*pretest';
+    replace_str = ['*Jc ' num2str(Jc,'%10.3f')];
+    txt_cell{20,1} = replace_str;
+    replace_str = ['*phi_crit ' num2str(crit_phi,'%10.3f')];
+    txt_cell{21,1} = replace_str;
+end
 %
 %write test analysis values
- if interp.cb_eval_test == 1
-     tear_load = result.tear_load;
-     tear_angle = result.tear_angle;
-     force = testdata.force;
-     CMOD = testdata.CMOD;
-     size_data = length(force);
-     txt_cell{18,1} = '%---Test Evaluation----%';
-     txt_cell{19,1} = '*test_eval';
-     replace_str = ['*tear_force ' num2str(tear_load,'%10.3f')];
-     txt_cell{20,1} = replace_str;
-     replace_str = ['*tear_phi ' num2str(tear_angle,'%10.3f')];
-     txt_cell{21,1} = replace_str;
-     txt_cell{22,1} = '*CMOD	Force';
-     for j = 1:size_data
-         replace_str = [num2str(CMOD(j),'%14.6f'), '  ' num2str(force(j),'%14.4f')];
-     txt_cell{22+j,1} = replace_str;
-     end
-      txt_cell{22+size_data+1,1} = '*end_test_data';
- end
+if interp.cb_eval_test == 1
+    tear_load = result.tear_load;
+    tear_angle = result.tear_angle;
+    force = testdata.force;
+    CMOD = testdata.CMOD;
+    size_data = length(force);
+    txt_cell{18,1} = '%---Test Evaluation----%';
+    txt_cell{19,1} = '*test_eval';
+    replace_str = ['*tear_force ' num2str(tear_load,'%10.3f')];
+    txt_cell{20,1} = replace_str;
+    replace_str = ['*tear_phi ' num2str(tear_angle,'%10.3f')];
+    txt_cell{21,1} = replace_str;
+    txt_cell{22,1} = '*CMOD	Force';
+    for j = 1:size_data
+        replace_str = [num2str(CMOD(j),'%14.6f'), '  ' num2str(force(j),'%14.4f')];
+        txt_cell{22+j,1} = replace_str;
+    end
+    txt_cell{22+size_data+1,1} = '*end_test_data';
+end
 
 
 %%
- %create txt file with *.ntrp input data
+%create txt file with *.ntrp input data
 
- TxtName = strcat(file_name,'_inputs.ntrp');
- %TxtName = 'try.txt';
- TXT = fopen(TxtName,'w');
- replace_str = [];
+TxtName = strcat(file_name,'_inputs.ntrp');
+%TxtName = 'try.txt';
+TXT = fopen(TxtName,'w');
+% replace_str = [];
 %
 for i = 1:length(txt_cell)
     replace_str = char(txt_cell(i,1));
-   fprintf(TXT,'%s\r\n',replace_str); 
+    fprintf(TXT,'%s\r\n',replace_str);
 end
-TXT = fclose(TXT);
+fclose(TXT);
 end
